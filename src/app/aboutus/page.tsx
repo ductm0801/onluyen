f1de83728d5cf488163c1f88f0354f500d23d6b4
đức trần <ductm@xherozone.com>
2024-10-13 18:52:53 +0700

    first commit

---
+++ src/app/aboutus/page.tsx
@@ -0,0 +1,9 @@
+const aboutus = () => {
+  return (
+    <div className="text-red-50 bg-black">
+      <h1>About Us</h1>
+      <p>This is the about us page.</p>
+    </div>
+  );
+};
+export default aboutus;
---
+++ src/interceptor/index.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+const axiosClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_HOST,
+  headers: {
+    "content-type": "application/json",
+  },
+});
+
+axiosClient.interceptors.request.use(
+  async (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (!accessToken) return config;
+
+    config.headers.Authorization = `Bearer ${accessToken}`;
+
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (
+      (error.response && error.response.status === 401) ||
+      (error.response && error.response.status === "username_is_not_existed")
+    ) {
+      localStorage.removeItem("accessToken");
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosClient;
