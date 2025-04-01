922a0ac504200c7803ecd85c91e1dc674baa522c
ductm0801 <ductm@xherozone.com>
2025-04-01 17:42:50 +0700

    instructor balance

--- src/views/InstructorDashboard/index.tsx
+++ src/views/InstructorDashboard/index.tsx
@@ -11,19 +11,20 @@ const InstructorDashboard = () => {
         <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
           <h4 className="font-bold text-gray-800 text-title-sm dark:text-white/90">
             Tổng số dư{" "}
+            {user?.instructor.availableBalance.toLocaleString("vi-VN")}đ
           </h4>
           <div className="flex items-end justify-between mt-4 sm:mt-5">
             <div>
               <p className="text-gray-700 text-theme-sm dark:text-gray-400">
-                Active Deal
+                Tăng
               </p>
             </div>
             <div className="flex items-center gap-1">
               <span className="inline-flex items-center px-2.5 py-0.5 justify-center gap-1 rounded-full font-medium text-theme-xs bg-success-50 text-success-600 dark:bg-success-500/15 dark:text-success-500">
-                +20%
+                20%
               </span>
               <span className="text-gray-500 text-theme-xs dark:text-gray-400">
-                last month
+                so với tháng trươcs
               </span>
             </div>
           </div>
