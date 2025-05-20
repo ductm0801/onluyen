ed40a8875a1d8bc6ef6bc2ac9e0892636a0038c6
ductm0801 <ductm@xherozone.com>
2025-05-20 16:58:23 +0700

    admin transaction log

--- src/views/DashBoard/index.tsx
+++ src/views/DashBoard/index.tsx
@@ -1,9 +1,12 @@
 "use client";
+import Paging from "@/components/Paging";
+import { statusEnum } from "@/constants/enum";
 import { IMAGES } from "@/constants/images";
 import { useLoading } from "@/providers/loadingProvider";
 import {
   getAdminRevenue,
   getAdminTransactionDashboard,
+  getPlatformFee,
   sentFormtoMail,
 } from "@/services";
 import { Button, Rate } from "antd";
@@ -24,12 +27,62 @@ const tableHeaders = [
   "isTakeExam",
 ];
 
+const cols = [
+  {
+    name: "Tên giao dịch",
+    className:
+      "px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white rounded-s-lg",
+  },
+  {
+    name: "Người thực hiện",
+    className:
+      "px-6 py-4 font-medium text-center text-gray-900 whitespace-nowrap dark:text-white",
+  },
+  {
+    name: "Số tiền",
+    className:
+      "px-6 py-4 font-medium text-end text-gray-900 whitespace-nowrap dark:text-white",
+  },
+  {
+    name: "Trạng thái",
+    className:
+      "px-6 py-4 font-medium text-center text-gray-900 whitespace-nowrap dark:text-white",
+  },
+  {
+    name: "Ngày thực hiện",
+    className:
+      "px-6 py-4 font-medium text-center text-gray-900 whitespace-nowrap dark:text-white rounded-e-lg",
+  },
+];
+
+export const renderBgColorStatus = (status: keyof typeof statusEnum) => {
+  switch (status) {
+    case 1:
+      return "from-orange-600 to-orange-300";
+    case 2:
+      return "from-orange-600 to-orange-300";
+    case 4:
+      return "from-red-600 to-red-300";
+    case 3:
+      return "from-emerald-600 to-emerald-400";
+    case 0:
+      return "from-slate-600 to-slate-300";
+    default:
+      return "from-emerald-500 to-teal-400";
+  }
+};
+
 const DashBoard = () => {
   const { setLoading } = useLoading();
   const [filterType, setFilterType] = useState("month");
   const [analyzeData, setAnalyzeData] = useState<any[]>([]);
   const [revenueData, setRevenueData] = useState<any>({});
   const [reviewData, setReviewData] = useState<any[]>([]);
+  const [transaction, setTransaction] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const [totalItems, setTotalItems] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   const fetchData = async () => {
     setLoading(true);
     try {
@@ -230,7 +283,23 @@ const DashBoard = () => {
   };
   useEffect(() => {
     fetchGgChart();
+    fetchPlatformTransaction();
   }, []);
+
+  const fetchPlatformTransaction = async () => {
+    try {
+      setLoading(true);
+      const res = await getPlatformFee(currentPage, pageSize);
+      setTransaction(res.data.items);
+      setTotalItems(res.data.totalItemsCount);
+      setTotalPages(res.data.totalPageCount);
+    } catch (err) {
+      setLoading(false);
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div>
       <div className="flex flex-col gap-4">
@@ -294,7 +363,7 @@ const DashBoard = () => {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-5 gap-4">
+      <div className="grid grid-cols-5 mt-4 gap-4">
         <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
           <h4 className="font-bold text-center text-gray-800 text-title-sm dark:text-white/90">
             Doanh thu <br />{" "}
@@ -345,6 +414,100 @@ const DashBoard = () => {
         </button>
       </div>
       <Bar options={options} data={data} />
+      <div className="relative overflow-x-auto">
+        <div className="flex justify-between items-center  flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4">
+          {/* <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="text"
+              id="table-search-users"
+              className="block p-2 ps-10 text-sm focus:ring-0 focus:outline-none text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
+              placeholder="Tìm kiếm"
+            />
+          </div> */}
+        </div>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              {cols.map((col, idx) => (
+                <th scope="col" className={col.className} key={idx}>
+                  {col.name}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {transaction &&
+              transaction.map((a: any, idx) => (
+                <tr
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
+                  key={idx}
+                >
+                  <th
+                    scope="row"
+                    className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    <div className="text-base font-semibold">
+                      {a?.transactionName}
+                    </div>
+                  </th>
+                  <td className="px-6 py-4 text-center">{a?.fullName}</td>
+                  <td
+                    className={`px-6 py-4 text-end font-bold ${
+                      a.transactionType === "Withdraw"
+                        ? "text-red-500"
+                        : "text-green-500"
+                    }`}
+                  >
+                    {a.transactionType === "Withdraw" ? "-" : "+"}
+                    {a.amount.toLocaleString("vi-VN")}đ
+                  </td>
+                  <td
+                    className={`p-2 text-sm leading-normal text-center align-middle shadow-transparent`}
+                  >
+                    <span
+                      className={`px-2 text-sm rounded-lg py-1 font-bold text-white bg-gradient-to-tl ${renderBgColorStatus(
+                        a.status
+                      )}`}
+                    >
+                      {statusEnum[a.status as keyof typeof statusEnum] ||
+                        "Unknown Status"}
+                    </span>
+                  </td>
+                  <td className="px-6 py-4 flex flex-col  items-center">
+                    <p>{dayjs(a.transactionDate).format("DD/MM/YYYY")}</p>
+                    <p className="text-xs">
+                      {dayjs(a.transactionDate).format("HH:mm:ss")}
+                    </p>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        <Paging
+          pageSize={pageSize}
+          currentPage={currentPage}
+          totalItems={totalItems}
+          totalPages={totalPages}
+          setCurrentPage={setCurrentPage}
+        />
+      </div>
     </div>
   );
 };
